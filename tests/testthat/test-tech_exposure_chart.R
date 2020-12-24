12f7699eaf2e6674381ff3301ec2cde40995430e
CJ Yetman <cj@2degrees-investing.org>
2020-12-24 12:12:54 +0100

    add minimal test of as_tech_exposure_data()

---
+++ tests/testthat/test-tech_exposure_chart.R
@@ -0,0 +1,50 @@
+test_that("as_tech_exposure_data() works", {
+  load("example-data/test.rda")
+  tech_exposure <-
+    as_tech_exposure_data(
+      investor_name = investor_name,
+      portfolio_name = portfolio_name,
+      start_year = start_year,
+      peer_group = peer_group,
+      equity_results_portfolio = equity_results_portfolio,
+      bonds_results_portfolio = bonds_results_portfolio,
+      indices_equity_results_portfolio = indices_equity_results_portfolio,
+      indices_bonds_results_portfolio = indices_bonds_results_portfolio,
+      peers_equity_results_portfolio = peers_equity_results_portfolio,
+      peers_bonds_results_portfolio = peers_bonds_results_portfolio,
+      green_techs = green_techs,
+      select_scenario = select_scenario,
+      select_scenario_auto = select_scenario_auto,
+      select_scenario_shipping = select_scenario_shipping,
+      select_scenario_other = select_scenario_other,
+      all_tech_levels = all_tech_levels,
+      equity_market_levels = equity_market_levels,
+      dataframe_translations = dataframe_translations,
+      language_select = "EN"
+    )
+
+  expect_error(
+    as_tech_exposure_data(
+      investor_name = investor_name,
+      portfolio_name = portfolio_name,
+      start_year = start_year,
+      peer_group = peer_group,
+      equity_results_portfolio = equity_results_portfolio,
+      bonds_results_portfolio = bonds_results_portfolio,
+      indices_equity_results_portfolio = indices_equity_results_portfolio,
+      indices_bonds_results_portfolio = indices_bonds_results_portfolio,
+      peers_equity_results_portfolio = peers_equity_results_portfolio,
+      peers_bonds_results_portfolio = peers_bonds_results_portfolio,
+      green_techs = green_techs,
+      select_scenario = select_scenario,
+      select_scenario_auto = select_scenario_auto,
+      select_scenario_shipping = select_scenario_shipping,
+      select_scenario_other = select_scenario_other,
+      all_tech_levels = all_tech_levels,
+      equity_market_levels = equity_market_levels,
+      dataframe_translations = dataframe_translations,
+      language_select = "EN"
+    )
+    , NA)
+  expect_s3_class(tech_exposure, "data.frame")
+})
