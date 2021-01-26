273d05294a0ecd899af6f54473640853d67a9c51
CJ Yetman <cj@2degrees-investing.org>
2021-01-26 12:23:25 +0100

    use jquerylib closes #5

--- R/trajectory_alignmemt_chart.R
+++ R/trajectory_alignmemt_chart.R
@@ -19,7 +19,7 @@ trajectory_alignment_chart <-
       list(
         system.file("js/trajectory_alignment.js", package = "r2dii.interactive"),
         system.file("js/d3-array.v2.min.js", package = "r2dii.interactive"),
-        system.file("js/jquery-3.5.1.js", package = "r2dii.interactive"),
+        jquerylib::jquery_core(major_version = 3),
         system.file("js/text_dropdown_jiggle.js", package = "r2dii.interactive"),
         system.file("css/2dii_gitbook_style.css", package = "r2dii.interactive"),
         system.file("css/hide_styles.css", package = "r2dii.interactive")
