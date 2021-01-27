63c623066fdf5beba3011b9d21c2e0c57dde1231
CJ Yetman <cj@cjyetman.com>
2021-01-27 09:29:04 +0100

    add the most basic tests

---
+++ tests/testthat/setup.R
@@ -0,0 +1,2 @@
+load(testthat::test_path("example-data/test.rda"))
+bonds_results_map$asset_class <- "Corporate Bonds"
--- tests/testthat/test-tech_exposure_chart.R
+++ tests/testthat/test-tech_exposure_chart.R
@@ -1,5 +1,23 @@
 test_that("as_tech_exposure_data() works", {
-  load("example-data/test.rda")
+  expect_error(
+    tech_exposure <-
+      as_tech_exposure_data(
+        equity_results_portfolio = equity_results_portfolio,
+        bonds_results_portfolio = bonds_results_portfolio,
+        indices_equity_results_portfolio = indices_equity_results_portfolio,
+        indices_bonds_results_portfolio = indices_bonds_results_portfolio,
+        select_scenario = select_scenario,
+        select_scenario_auto = select_scenario_auto,
+        select_scenario_shipping = select_scenario_shipping,
+        select_scenario_other = select_scenario_other,
+        equity_market_levels = equity_market_levels
+      ),
+    NA
+  )
+  expect_s3_class(tech_exposure, "data.frame")
+})
+
+test_that("tech_exposure_chart() works", {
   tech_exposure <-
     as_tech_exposure_data(
       equity_results_portfolio = equity_results_portfolio,
@@ -13,19 +31,6 @@ test_that("as_tech_exposure_data() works", {
       equity_market_levels = equity_market_levels
     )
 
-  expect_error(
-    as_tech_exposure_data(
-      equity_results_portfolio = equity_results_portfolio,
-      bonds_results_portfolio = bonds_results_portfolio,
-      indices_equity_results_portfolio = indices_equity_results_portfolio,
-      indices_bonds_results_portfolio = indices_bonds_results_portfolio,
-      select_scenario = select_scenario,
-      select_scenario_auto = select_scenario_auto,
-      select_scenario_shipping = select_scenario_shipping,
-      select_scenario_other = select_scenario_other,
-      equity_market_levels = equity_market_levels
-    ),
-    NA
-  )
-  expect_s3_class(tech_exposure, "data.frame")
+  expect_error(tech_exposure_obj <- tech_exposure_chart(tech_exposure), NA)
+  expect_s3_class(tech_exposure_obj, "r2d3")
 })
